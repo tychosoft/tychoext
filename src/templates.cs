1eb8ddafde98b7ef9e25a323a687d14eebfcf5f4
David Sugar <tychosoft@gmail.com>
2025-01-09 08:27:30 -0500

    Some generic templates

---
+++ src/templates.cs
@@ -0,0 +1,16 @@
+// Copyright (C) 2025 Tycho Softworks.
+// This code is licensed under MIT license.
+
+namespace Tychosoft {
+    public static class Templates {
+        public static bool InRange<T>(this T value, T min, T max) where T : IComparable<T> {
+            return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+        }
+
+        public static T Clamp<T>(this T value, T min, T max) where T : IComparable<T> {
+            if (value.CompareTo(min) < 0) { return min; }
+            if (value.CompareTo(max) > 0) { return max; }
+            return value;
+        }
+    }
+} // end namespace
